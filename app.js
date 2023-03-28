b7ea3af90b346578cb2a29fd4b74535f6c5bc5c0
Victor Manuel Romero Baez <vmrbaez@gmail.com>
2023-03-28 13:25:41 -0600

    Se hicieron todas las vistas y es completamente funcional

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
+require('dotenv').config();
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
+const port = process.env.PORT;
+
+//Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 // servir estatico 
 app.use( express.static('public') );
 
-
+app.get('/', (req, res) => {
+    res.render('home',{
+        nombre:'Victor',
+        titulo:'App node'
+    });
+});
 
 app.get('/generic', (req, res) => {
-    res.sendFile(__dirname + '/public/generic.html')
+    res.render('generic',{
+        nombre:'Victor',
+        titulo:'App node'
+    });
 });
 
 app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html')
+    res.render('elements',{
+        nombre:'Victor',
+        titulo:'App Node'
+    });
 });
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/404.html')
 });
 
-app.listen(8080)
+app.listen(port, () => {
+    console.log(`Example app listening at localhost:${port}`);
+});
